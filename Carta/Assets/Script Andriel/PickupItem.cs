afa7f77eb294ab652085bd263b78277cad3ad87f
Andriel <167346031+Farinha67@users.noreply.github.com>
2026-05-07 09:27:58 -0300

    mecaniica de caixa emplementada

---
+++ Carta/Assets/Script Andriel/PickupItem.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+public class PickupItem : MonoBehaviour
+{
+    public Transform handPoint;
+    public float pickupRange = 5f;
+
+    private GameObject heldItem;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (heldItem == null)
+            {
+                TryPickup();
+            }
+            else
+            {
+                DropItem();
+            }
+        }
+    }
+
+    void TryPickup()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit, pickupRange))
+        {
+            Debug.Log("Acertou: " + hit.collider.name);
+
+            if (hit.collider.CompareTag("Pickup"))
+            {
+                heldItem = hit.collider.gameObject;
+
+                Rigidbody rb = heldItem.GetComponent<Rigidbody>();
+
+                if (rb != null)
+                {
+                    rb.isKinematic = true;
+                }
+
+                heldItem.transform.position = handPoint.position;
+                heldItem.transform.parent = handPoint;
+            }
+        }
+    }
+
+    void DropItem()
+    {
+        Rigidbody rb = heldItem.GetComponent<Rigidbody>();
+
+        heldItem.transform.parent = null;
+
+        if (rb != null)
+        {
+            rb.isKinematic = false;
+        }
+
+        heldItem = null;
+    }
+}
